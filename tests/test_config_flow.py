e16cb9da2fa8b25954f27b4ca8948214ac081ecb
Daniel Raper <me@danr.uk>
2023-12-28 22:54:41 +0000

    First passing test!

--- tests/test_config_flow.py
+++ tests/test_config_flow.py
@@ -7,11 +7,22 @@ from pytest_homeassistant_custom_component.common import MockConfigEntry
 from custom_components.ohme import config_flow
 from custom_components.ohme.const import DOMAIN
 
-@pytest.mark.asyncio
 async def test_step_account(hass):
     """Test the initialization of the form in the first step of the config flow."""
-    # result = await hass.config_entries.flow.async_step_user(
-    #     None
-    # )
+    result = await hass.config_entries.flow.async_init(
+        config_flow.DOMAIN, context={"source": "user"}
+    )
+    
+    expected =  {
+            'type': 'form',
+            'flow_id': mock.ANY,
+            'handler': 'ohme',
+            'step_id': 'user',
+            'data_schema': config_flow.USER_SCHEMA,
+            'errors': {},
+            'description_placeholders': None,
+            'last_step': None,
+            'preview': None
+    }
 
     assert expected == result

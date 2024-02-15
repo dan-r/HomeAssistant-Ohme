d9d3c4349ea3d1fed97ea9eb01afdc8b2ab40ce8
Daniel Raper <me@danr.uk>
2024-02-15 16:48:09 +0000

    Version bump for breaking changes

--- custom_components/ohme/const.py
+++ custom_components/ohme/const.py
@@ -1,7 +1,7 @@
 """Component constants"""
 DOMAIN = "ohme"
 USER_AGENT = "dan-r-homeassistant-ohme"
-INTEGRATION_VERSION = "0.6.2"
+INTEGRATION_VERSION = "0.7.0"
 CONFIG_VERSION = 1
 ENTITY_TYPES = ["sensor", "binary_sensor", "switch", "button", "number", "time"]
 

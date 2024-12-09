f4c47fca3b25e2791f546e5dcebdae2512b2d36c
Daniel Raper <me@danr.uk>
2024-12-09 17:37:20 +0000

    Fix binary sensor bug and improve config flow tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,52 @@
-"""Global fixtures for custom integration."""
+"""Provide common fixtures."""
 
 import pytest
-import pytest_socket
-
+from unittest.mock import AsyncMock, patch
+import asyncio
+from tests.test_util.aiohttp import AiohttpClientMocker
+from tests.common import MockConfigEntry, load_json_value_fixture
+from custom_components.ohme.const import DOMAIN
+import json
 
 @pytest.fixture(autouse=True)
 def auto_enable_custom_integrations(enable_custom_integrations):
     """Enable custom integrations defined in the test dir."""
     yield
 
+@pytest.fixture(name="mock_session")
+def mock_session():
+    """Mock aiohttp.ClientSession."""
+    mocker = AiohttpClientMocker()
 
-def enable_external_sockets():
-    """Enable external sockets for custom integrations."""
-    pytest_socket.enable_socket()
+    mocker.post(
+        "https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword",
+        json={"idToken":"", "refreshToken":""}
+    )
+
+    mocker.get(
+        "/v1/users/me/account",
+        json=load_json_value_fixture("account.json", DOMAIN)
+    )
+
+    mocker.get(
+        "/v1/chargeSessions",
+        json=load_json_value_fixture("charge_sessions.json", DOMAIN)
+    )
+
+    mocker.get(
+        "/v1/chargeRules",
+        json=load_json_value_fixture("charge_rules.json", DOMAIN)
+    )
+
+    mocker.get(
+        "/v1/chargeDevices/chargerid/advancedSettings",
+        json=load_json_value_fixture("advanced_settings.json", DOMAIN)
+    )
+
+    with patch(
+        "aiohttp.ClientSession",
+        side_effect=lambda *args, **kwargs: mocker.create_session(
+            asyncio.get_event_loop()
+        ),
+    ):
+        yield mocker
